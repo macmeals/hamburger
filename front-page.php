bd84d5436e9133e378010024dc778eb4e126d71b
macmeals <68972494+macmeals@users.noreply.github.com>
2021-03-30 23:00:05 +0900

    サイドバーの共通部分の切り出し完了

--- front-page.php
+++ front-page.php
@@ -44,41 +44,9 @@
       </div>
     </div>
   </article>
-   
-  <aside class="l-aside">
-    <ul class="p-aside__main">
-      <li>バーガー
-        <ul class="p-aside__sub">
-          <li>ハンバーガー</li>
-          <li>チーズバーガー</li>
-          <li>テリヤキバーガー</li>
-          <li>アボガドバーガー</li>
-          <li>フィッシュバーガー</li>
-          <li>ベーコンバーガー</li>
-          <li>チキンバーガー</li>
-        </ul>
-      </li>
-      <li>サイド
-        <ul class="p-aside__sub">
-          <li>ポテト</li>
-          <li>サラダ</li>
-          <li>ナゲット</li>
-          <li>コーン</li>
-        </ul>
-      </li>
-      <li>ドリンク
-        <ul class="p-aside__sub">
-          <li>コーラ</li>
-          <li>ファンタ</li>
-          <li>オレンジ</li>
-          <li>アップル</li>
-          <li>紅茶(Ice/Hot)</li>
-          <li>コーヒー(Ice/Hot)</li>
-        </ul>
-      </li>
-    </ul>
   
-  </aside>
+  <!-- sidebar部分の読み込み -->
+  <?php include ('sidebar.php'); ?>
 
  </main>
 
