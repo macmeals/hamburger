2202b5cf3ae56380fb03056afd398c7540c855c8
macmeals <68972494+macmeals@users.noreply.github.com>
2021-04-01 23:47:01 +0900

    ・Top画像を実装 ・Top後のテキストを実装 ・バーガー紹介の骨組みを作成

---
+++ archive.php
@@ -0,0 +1,64 @@
+<!-- Head及びheader部分の読み込み -->
+<?php include ('header.php'); ?>
+
+<main class ="l-main">
+    <article class="p-archive">
+       <div class="p-archive__top u-padding__left__right">
+           <h2>Menu:</h2>
+           <p>チーズバーガー</p>
+       </div>
+       <div class="p-archive__text">
+           <h3>小見出しが入ります</h3>
+           <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+       </div>
+       <div class="p-archive__boxs">
+           <div class="p-archive__box1"></div>
+           <div class="p-archive__box2">
+              <h2>チーズバーガー</h3>
+              <h3>小見出しが入ります</h3>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+              <div class=c-circlebutton>
+                 詳しく見る
+              </div>
+           </div>
+       </div>
+       <div class="p-archive__boxs">
+           <div class="p-archive__box1"></div>
+           <div class="p-archive__box2">
+              <h2>ダブルチーズバーガー</h3>
+              <h3>小見出しが入ります</h3>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+              <div class=c-circlebutton>
+                 詳しく見る
+              </div>
+           </div>
+       </div>
+       <div class="p-archive__boxs">
+           <div class="p-archive__box1"></div>
+           <div class="p-archive__box2">
+              <h2>スペシャルチーズバーガー</h3>
+              <h3>小見出しが入ります</h3>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+              <div class=c-circlebutton>
+                 詳しく見る
+              </div>
+           </div>
+       </div>
+
+
+
+
+
+
+
+
+
+    </article>
+
+    <!-- sidebar部分の読み込み -->
+    <?php include ('sidebar.php'); ?>
+
+</main>
+
+ <!-- footer部分の読み込み -->
+ <?php include('footer.php'); ?>
