f4554892553bf2889c3f6d86984fd87769aea276
macmeals <68972494+macmeals@users.noreply.github.com>
2021-04-04 21:47:22 +0900

    PC版のMainページの完成

--- archive.php
+++ archive.php
@@ -3,56 +3,66 @@
 
 <main class ="l-main">
     <article class="p-archive">
-       <div class="p-archive__top u-padding__left__right">
-           <h2>Menu:</h2>
-           <p>チーズバーガー</p>
-       </div>
-       <div class="p-archive__text">
-           <h3>小見出しが入ります</h3>
-           <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-       </div>
-       <div class="p-archive__boxs">
+       <div class="p-archive__top">
+           <div class="p-archive__menu u-padding__left__right">
+              <h2>Menu:</h2>
+              <p>チーズバーガー</p>
+           </div>
+       </div>    
+       <div class="p-archive__textall">
+           <div class="p-archive__text">
+              <h3>小見出しが入ります</h3>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+          </div> 
+      </div>
+       <div class="p-archive__boxes">
            <div class="p-archive__box1"></div>
            <div class="p-archive__box2">
               <h2>チーズバーガー</h3>
               <h3>小見出しが入ります</h3>
               <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-              <div class=c-circlebutton>
+              <button class=c-circlebutton>
                  詳しく見る
-              </div>
+              </button>
            </div>
        </div>
-       <div class="p-archive__boxs">
+       <div class="p-archive__boxes">
            <div class="p-archive__box1"></div>
            <div class="p-archive__box2">
               <h2>ダブルチーズバーガー</h3>
               <h3>小見出しが入ります</h3>
               <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-              <div class=c-circlebutton>
+              <button class=c-circlebutton>
                  詳しく見る
-              </div>
+              </button>
            </div>
        </div>
-       <div class="p-archive__boxs">
+       <div class="p-archive__boxes">
            <div class="p-archive__box1"></div>
            <div class="p-archive__box2">
               <h2>スペシャルチーズバーガー</h3>
               <h3>小見出しが入ります</h3>
               <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-              <div class=c-circlebutton>
+              <button class=c-circlebutton>
                  詳しく見る
-              </div>
+              </button>
            </div>
        </div>
-
-
-
-
-
-
-
-
-
+       <ul class="p-pagination">
+           <li >page 1/10</li>
+           <li class="p-pagination__arrow">«</li>
+           <li class="p-pagination__link">1</li>
+           <li class="p-pagination__link">2</li>
+           <li class="p-pagination__link">3</li>
+           <li class="p-pagination__link">4</li>
+           <li class="p-pagination__link">5</li>
+           <li class="p-pagination__link">6</li>
+           <li class="p-pagination__link">7</li>
+           <li class="p-pagination__link">8</li>
+           <li class="p-pagination__link">9</li> 
+           <li class="p-pagination__arrow">»</li>
+       </ul>
+      
     </article>
 
     <!-- sidebar部分の読み込み -->
@@ -61,4 +71,5 @@
 </main>
 
  <!-- footer部分の読み込み -->
+
  <?php include('footer.php'); ?>
