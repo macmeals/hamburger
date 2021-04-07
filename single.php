b57fcd80a3ad8cdd852f6a7ed6bd059a0aed8e40
macmeals <68972494+macmeals@users.noreply.github.com>
2021-04-07 23:11:11 +0900

    ・Single.phpを作成 ・Single.phpの骨組みを

---
+++ single.php
@@ -0,0 +1,11 @@
+<!-- Head及びheader部分の読み込み -->
+<?php include ('header.php'); ?>
+
+<main class ="l-main">
+    <article class="p-single">
+    </article>
+
+     <!-- sidebar部分の読み込み -->
+    <?php include ('sidebar.php'); ?>
+
+</main>
